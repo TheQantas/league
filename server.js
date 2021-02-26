0f4881db53a0029ffbb15c9dff6b32a7b6434978
HailtotheVictors <44556872+HailtotheVictors@users.noreply.github.com>
2021-02-25 21:09:33 -0700

    Update server.js

--- server.js
+++ server.js
@@ -38,6 +38,39 @@ app.use('/audio', express.static(__dirname + '/audio'));
 const cookieParser = require('cookie-parser');
 var devices = {};
 
+//block
+
+var url = require('url');
+var WebSocket = require('ws');
+var HttpsProxyAgent = require('https-proxy-agent');
+
+// HTTP/HTTPS proxy to connect to
+var proxy = process.env.http_proxy || 'http://168.63.76.32:3128';
+console.log('using proxy server %j', proxy);
+
+// WebSocket endpoint for the proxy to connect to
+var endpoint = process.argv[2] || 'ws://echo.websocket.org';
+var parsed = url.parse(endpoint);
+console.log('attempting to connect to WebSocket %j', endpoint);
+
+// create an instance of the `HttpsProxyAgent` class with the proxy server information
+var options = url.parse(proxy);
+
+var agent = new HttpsProxyAgent(options);
+
+// finally, initiate the WebSocket connection
+var socket = new WebSocket(endpoint, { agent: agent });
+
+socket.on('open', function () {
+  console.log('"open" event!');
+  socket.send('hello world');
+});
+
+socket.on('message', function (data, flags) {
+  console.log('"message" event! %j %j', data, flags);
+  socket.close();
+});
+
 //time code
 
 const tiempo = {};
